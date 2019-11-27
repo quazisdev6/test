5f0e342c178c7628de505ad9b3b22e0ea250cd68
quazisharar <quazis@dev6.com>
2019-11-27 11:25:08 -0500

    add static query for team members

--- src/components/team/team-section.js
+++ src/components/team/team-section.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./team-section.css"
 import TeamMember from "./team-member/team-member"
+import { StaticQuery } from "gatsby";
 
 export default () => (
     <section className="section_all" id="team">
@@ -19,103 +20,50 @@ export default () => (
             </div>
 
             <div className="row mt-5">
-              {testArray.map((teamMember) => {
-                return (
-                  <TeamMember name={teamMember.name} title={teamMember.title} description={teamMember.description}/>
-                )
-              })}
-                {/*<div className="col-lg-3">*/}
-                    {/*<div className="landing_team_box bg-white text-center mt-3">*/}
-                        {/*<img src="images/team/team-1.jpg" alt="" className=" img-fluid mx-auto d-block"/>*/}
-                            {/*<div className="mt-3 buss_team_info p-3">*/}
-                                {/*<h6 className="">Ronnie M. Ricketts</h6>*/}
-                                {/*<small className="text-muted font-weight-normal">CEO/Founder</small>*/}
-                                {/*<p className="mb-0 text-muted">It is a long established fact that a reader will be*/}
-                                    {/*distracted</p>*/}
-                                {/*<div className="mt-3 mb-3">*/}
-                                    {/*<ul className="list-inline business_team_social mb-0 text-center">*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-facebook"></i></a></li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-google"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-twitter"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-instagram"></i></a></li>*/}
-                                    {/*</ul>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
+              {/*{testArray.map((teamMember) => {*/}
+                {/*return (*/}
+                  {/*<TeamMember name={teamMember.name} title={teamMember.title} description={teamMember.description}/>*/}
+                {/*)*/}
+              {/*})}*/}
 
-                {/*<div className="col-lg-3">*/}
-                    {/*<div className="landing_team_box bg-white text-center mt-3">*/}
-                        {/*<img src="images/team/team-2.jpg" alt="" className=" img-fluid mx-auto d-block"/>*/}
-                            {/*<div className="mt-3 buss_team_info p-3">*/}
-                                {/*<h6 className="">Robert E. Ricks</h6>*/}
-                                {/*<small className="text-muted font-weight-normal">CEO/Founder</small>*/}
-                                {/*<p className="mb-0 text-muted">It is a long established fact that a reader will be*/}
-                                    {/*distracted</p>*/}
-                                {/*<div className="mt-3 mb-3">*/}
-                                    {/*<ul className="list-inline business_team_social mb-0 text-center">*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-facebook"></i></a></li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-google"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-twitter"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-instagram"></i></a></li>*/}
-                                    {/*</ul>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
+              <StaticQuery
+                query={graphql`
+  {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "team-name"}}}) {
+      nodes {
+        frontmatter {
+          membername
+          membertitle
+          memberdescription
+        }
+      }
+    }
+  }
+`}
 
-                {/*<div className="col-lg-3">*/}
-                    {/*<div className="landing_team_box bg-white text-center mt-3">*/}
-                        {/*<img src="images/team/team-3.jpg" alt="" className=" img-fluid mx-auto d-block"/>*/}
-                            {/*<div className="mt-3 buss_team_info p-3">*/}
-                                {/*<h6 className="">Jennifer M. Marsh</h6>*/}
-                                {/*<small className="text-muted font-weight-normal">CEO/Founder</small>*/}
-                                {/*<p className="mb-0 text-muted">It is a long established fact that a reader will be*/}
-                                    {/*distracted</p>*/}
-                                {/*<div className="mt-3 mb-3">*/}
-                                    {/*<ul className="list-inline business_team_social mb-0 text-center">*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-facebook"></i></a></li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-google"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i className="mdi mdi-twitter"></i></a>*/}
-                                        {/*</li>*/}
-                                        {/*<li className="list-inline-item"><a href="#"><i*/}
-                                            {/*className="mdi mdi-instagram"></i></a></li>*/}
-                                    {/*</ul>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
+                render={ (data) => {
+                  return data.allMarkdownRemark.nodes.map((val) => {
+                    console.log(val);
+                    return (
+                      <TeamMember name={val.frontmatter.membername}
+                                  title={val.frontmatter.membertitle}
+                                  description={val.frontmatter.memberdescription}
+                      />
+                    )
+                  })
 
-                {/*<div className="col-lg-3">*/}
-                    {/*<div className="landing_team_box bg-white text-center mt-3">*/}
-                        {/*<img src="images/team/team-4.jpg" alt="" className=" img-fluid mx-auto d-block"/>*/}
-                            {/*<div className="mt-3 buss_team_info p-3">*/}
-                                {/*<h6 className="">James C. Spence</h6>*/}
-                                {/*<small className="text-muted font-weight-normal">CEO/Founder</small>*/}
-                                {/*<p className="mb-0 text-muted">It is a long established fact that a reader will be*/}
-                                    {/*distracted</p>*/}
-                            {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
+                } }
+              ></StaticQuery>
+
             </div>
         </div>
     </section>
 )
 
 
-let testArray = [
-  {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
-  {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
-  {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
-  {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"}
-]
+// let testArray = [
+//   {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
+//   {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
+//   {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"},
+//   {name: "Test name", title: "Lorem IpsumLorem", description: "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum"}
+// ]
