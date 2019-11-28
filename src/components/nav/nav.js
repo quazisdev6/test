585b70c2e1dce76d7c1d4854bdd44649e94a7657
quazisharar <quazis@dev6.com>
2019-11-27 23:55:02 -0500

    add the custom scripts

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,9 +1,25 @@
 import React from "react"
 import "./nav.css"
+import { Helmet } from 'react-helmet'
+import { withPrefix } from "gatsby"
+
 
 export default () => (
+
     <nav className="navbar navbar-expand-lg fixed-top custom_nav_menu sticky">
         <div className="container">
+            <Helmet>
+              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                      crossOrigin="anonymous"></script>
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                      crossOrigin="anonymous"></script>
+              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                      crossOrigin="anonymous"></script>
+              <script src={withPrefix('custom.js')} type="text/javascript" />
+            </Helmet>
             {/* LOGO */}
             <a className="navbar-brand logo" href="index.html">
                 <img src="images/logo-2.png" alt="" className="img-fluid logo-light" />
