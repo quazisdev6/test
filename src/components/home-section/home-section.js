23d729ec6c0be6a2674956f3b671fe79d72afe65
quazisharar <quazis@dev6.com>
2019-11-29 03:26:34 -0500

    add dynamic hero text and image

--- src/components/home-section/home-section.js
+++ src/components/home-section/home-section.js
@@ -3,24 +3,17 @@ import "./home-section.css"
 import { StaticQuery } from "gatsby";
 
 
-export default () => (
-    <section className="bg_home_cover_landing full_height_100vh_home" id="home">
-        <div className="bg_overlay_cover_on_landing" />
-        <div className="home_table_cell">
-            <div className="home_table_cell_center">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="text-center">
-                              <StaticQuery
-                                query={graphql`
+export default () => {
+
+  return <StaticQuery
+    query={graphql`
   {
     allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "index-page"}}}) {
       nodes {
         frontmatter {
           image {
             childImageSharp {
-              resize(width: 100, height: 100) {
+              resize(width: 2000, height: 1333) {
                 src
                 tracedSVG
                 width
@@ -40,21 +33,36 @@ export default () => (
   }
 `}
 
-                                render={ (data) => {
-                                  return data.allMarkdownRemark.nodes.map((val) => {
-                                    console.log(val);
-                                    return (
-                                      <h1 className="home_title_landing text-white mx-auto text-capitalize mb-0 pt-3">{val.frontmatter.heading}</h1>
-                                    )
-                                  })
-
-                                } }
-                              ></StaticQuery>
-                            </div>
-                        </div>
+    render={(data) => {
+      return data.allMarkdownRemark.nodes.map((val) => {
+        console.log(val);
+        const customStyle ={
+          backgroundImage: `url(${val.frontmatter.image ? val.frontmatter.image.childImageSharp.resize.src : ""})`
+        }
+        return (
+          <section
+            style={customStyle}
+            className="bg_home_cover_landing full_height_100vh_home" id="home">
+            <div className="bg_overlay_cover_on_landing"/>
+            <div className="home_table_cell">
+              <div className="home_table_cell_center">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="text-center">
+                        <h1
+                          className="home_title_landing text-white mx-auto text-capitalize mb-0 pt-3">{val.frontmatter.heading}</h1>
+                      </div>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-    </section>
-)
+          </section>
+        )
+      })
+
+    }}
+  ></StaticQuery>
+
+}
