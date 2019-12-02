57915fb828f5b18093cf0e3b0a1847381bca6ea3
quazisharar <quazis@dev6.com>
2019-12-02 00:43:38 -0500

    change title meta and add develop branch

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,39 +14,9 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/dca-favicon.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/dca-favicon.png`}
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
-
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
       </Helmet>
       <Nav/>
       <img src="/static/img/DCA31.png" alt="" className="img-fluid logo-light" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,23 @@ import TeamSection from "../components/team/team-section"
 import ProjectSection from "../components/projects-section/project-section"
 import ContactSection from "../components/contact-us/contact-us"
 import Footer from "../components/footer/footer"
-
+import Helmet from 'react-helmet'
+import useSiteMetadata from "../components/SiteMetadata";
 
 
 export default ({data}) => {
   console.log(data);
   const {clientblurb, serviceblurb, teamblurb, projectblurb} = data.allMarkdownRemark.nodes[0].frontmatter;
+  const { title, description } = useSiteMetadata();
   console.log(clientblurb, serviceblurb, teamblurb);
 
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
       <Nav/>
       <HomeSection/>
       <ServiceSection serviceBlurb={serviceblurb}/>
